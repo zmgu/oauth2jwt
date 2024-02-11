07e629d56dc52d5e5f48a440ce4bdd8b32859078
zmgu <manggu94@gmail.com>
2024-02-11 10:40:11 +0900

    Initial commit

---
+++ src/main/groovy/com/ex/oauth2jwt/Oauth2jwtApplication.groovy
@@ -0,0 +1,13 @@
+package com.ex.oauth2jwt
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Oauth2jwtApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(Oauth2jwtApplication, args)
+    }
+
+}
---
+++ src/test/groovy/com/ex/oauth2jwt/Oauth2jwtApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.ex.oauth2jwt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Oauth2jwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

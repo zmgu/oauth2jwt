4ea44c2bce72088aa4cb1d7980c806ea50137afc
zmgu <manggu94@gmail.com>
2024-02-12 11:01:41 +0900

    CustomOAuth2UserService 생성

---
+++ src/main/groovy/com/ex/oauth2jwt/service/CustomOAuth2UserService.java
@@ -0,0 +1,40 @@
+package com.ex.oauth2jwt.service;
+
+import com.ex.oauth2jwt.dto.GoogleResponse;
+import com.ex.oauth2jwt.dto.NaverResponse;
+import com.ex.oauth2jwt.dto.OAuth2Response;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        System.out.println(oAuth2User);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        OAuth2Response oAuth2Response = null;
+        if (registrationId.equals("naver")) {
+
+            oAuth2Response = new NaverResponse(oAuth2User.getAttributes());
+        }
+        else if (registrationId.equals("google")) {
+
+            oAuth2Response = new GoogleResponse(oAuth2User.getAttributes());
+        }
+        else {
+
+            return null;
+        }
+
+        //추후 작성
+        return null;
+    }
+}

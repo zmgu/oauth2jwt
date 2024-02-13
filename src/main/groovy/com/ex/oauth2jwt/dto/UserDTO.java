74c1273e3bf3c31479669ca6ace8191623be37aa
zmgu <manggu94@gmail.com>
2024-02-13 10:05:51 +0900

    UserDTO 생성

---
+++ src/main/groovy/com/ex/oauth2jwt/dto/UserDTO.java
@@ -0,0 +1,12 @@
+package com.ex.oauth2jwt.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UserDTO {
+
+    private String role;
+    private String name;
+    private String username;
+}

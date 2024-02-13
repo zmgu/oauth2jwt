f37574e2daeb4bada506f9a6cea9b6929ddf2021
zmgu <manggu94@gmail.com>
2024-02-13 10:08:01 +0900

    리소스 서버에서 발급 받은 정보로 사용자를 특정할 아이디값 생성

--- src/main/groovy/com/ex/oauth2jwt/service/CustomOAuth2UserService.java
+++ src/main/groovy/com/ex/oauth2jwt/service/CustomOAuth2UserService.java
@@ -1,8 +1,6 @@
 package com.ex.oauth2jwt.service;
 
-import com.ex.oauth2jwt.dto.GoogleResponse;
-import com.ex.oauth2jwt.dto.NaverResponse;
-import com.ex.oauth2jwt.dto.OAuth2Response;
+import com.ex.oauth2jwt.dto.*;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -34,7 +32,14 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             return null;
         }
 
-        //추후 작성
-        return null;
+        //리소스 서버에서 발급 받은 정보로 사용자를 특정할 아이디값을 만듬
+        String username = oAuth2Response.getProvider()+" "+oAuth2Response.getProviderId();
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUsername(username);
+        userDTO.setName(oAuth2Response.getName());
+        userDTO.setRole("ROLE_USER");
+
+        return new CustomOAuth2User(userDTO);
     }
 }

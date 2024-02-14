dfd8c64593f790372dd6d2c7d60a3999d7eb1a31
zmgu <manggu94@gmail.com>
2024-02-14 09:41:12 +0900

    UserRepository 생성

---
+++ src/main/groovy/com/ex/oauth2jwt/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ex.oauth2jwt.repository;
+
+import com.ex.oauth2jwt.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    UserEntity findByUsername(String username);
+}
